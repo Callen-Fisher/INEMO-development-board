abd6f1ac01961a92551542f55e39fc4bc643ce14
Roberto Aldera <raldera@users.noreply.github.com>
2017-07-27 10:03:50 +0200

    Add files via upload
    Source code bulk upload.

---
+++ src/control.h
@@ -0,0 +1,22 @@
+/*
+ * control.h
+ *
+ *  Created on: Jun 9, 2014
+ *      Author: Callen Fisher
+ */
+
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+#include "stm32f10x.h"
+#include <stddef.h>
+#include "AccMag.h"
+#include "gyro.h"
+#include "pwm.h"
+
+void readEncoder(float *shaft_revs, float *shaft_speed);
+void calibrateMEMS(float* acc, float* accCalib, float* gyro, float* gyroCalib, u8* temperature);
+void controlMethod(float*acc,float*gyro,uint8_t*temp,float*angles,float*velocities,float*positions,
+		float*PWMval,float*shaft_revs,uint8_t*activateControl);
+
+#endif /* CONTROL_H_ */
---
+++ src/controlLoopTimer.h
@@ -0,0 +1,18 @@
+/*
+ * controlLoopTimer.h
+ *
+ *  Created on: Jun 6, 2014
+ *      Author: Callen Fisher
+ */
+
+#ifndef CONTROLLOOPTIMER_H_
+#define CONTROLLOOPTIMER_H_
+
+#include "stm32f10x.h"
+#include <stddef.h>
+
+void setUpLoopTimer(void);
+void setUpEncoder(void);
+void waitForEndTimerHz(void);
+
+#endif /* CONTROLLOOPTIMER_H_ */
